efbbffddf521b0883d108ea780c1df6f8aea91ff
Biplop <biplopdangol123@gmail.com>
2022-02-15 15:12:20 +0545

    tic tac toe implemented

--- main.py
+++ main.py
@@ -1,2 +1,105 @@
+import random
 from tkinter import *
+
+def next_turn(row,column):
+    global player
+    if buttons[row][column]["text"] == "" and check_winner() is False:
+        if player == players[0]:
+            buttons[row][column]["text"] = player
+            if check_winner() is True:
+                label.config(text=f"{player} wins")
+            elif check_winner() is False:
+                player = players[1]
+                label.config(text=f"{player} turn")
+            elif check_winner() == "draw":
+                label.config(text="Game draw")
+        else:
+            buttons[row][column]["text"] = player
+            if check_winner() is True:
+                label.config(text=f"{player} wins")
+            elif check_winner() is False:
+                player = players[0]
+                label.config(text=f"{player} turn")
+            elif check_winner() == "draw":
+                label.config(text="Game draw")    
+
+
+
+def check_winner():
+    for row in range(3):
+        if buttons[row][0]["text"] == buttons[row][1]["text"] == buttons[row][2]["text"] != "":
+            buttons[row][0].config(bg="green")
+            buttons[row][1].config(bg="green")
+            buttons[row][2].config(bg="green")
+            return True
+    for column in range(3):
+        if buttons[0][column]["text"] == buttons[1][column]["text"] == buttons[2][column]["text"] != "":
+            buttons[0][column].config(bg="green")
+            buttons[1][column].config(bg="green")
+            buttons[2][column].config(bg="green")
+            return True
+    if buttons[0][0]["text"] == buttons[1][1]["text"] == buttons[2][2]["text"] != "":
+        buttons[0][0].config(bg="green")
+        buttons[1][1].config(bg="green")
+        buttons[2][2].config(bg="green")
+        return True
+    if buttons[0][2]["text"] == buttons[1][1]["text"] == buttons[2][0]["text"] != "":
+        buttons[0][2].config(bg="green")
+        buttons[1][1].config(bg="green")
+        buttons[2][0].config(bg="green")
+        return True
+    elif empty_spaces() is False:
+        for row in range(3):
+            for column in range(3):
+                buttons[row][column].config(bg="yellow")
+        return "draw"
+    else:
+        return False
+
+def empty_spaces():
+    spaces = 9
+    for row in range(3):
+        for column in range(3):
+            if buttons[row][column]["text"] != "":
+                spaces -= 1
+    if spaces == 0:
+        return False
+    else:
+        return True
+
+
+def new_game():
+    global player
+    player = random.choice(players)
+    label.config(text=f"{player} turn")
+    for row in range(3):
+        for column in range(3):
+            buttons[row][column].config(text="",bg="#F0F0F0")
+
+
 root = Tk()
+root.title("Tic Tac Toe")
+players = ["X","O"]
+player = random.choice(players)
+buttons = [[0,0,0],
+           [0,0,0],
+           [0,0,0]]
+label = Label(text= f"{player} turn",font=("consolas",40))
+label.pack(side="top")
+reset_btn = Button(text="Restart",font=("consolas",20),command=new_game)
+reset_btn.pack(side="top")
+frame = Frame(root) 
+frame.pack()
+for row in range(3):
+    for column in range(3):
+        
+        buttons[row][column] = Button(frame,text="",
+                                  font=("consolas",40),
+                                  width=5,
+                                  height=2,
+                                  command= lambda row=row,column=column : next_turn(row,column))
+        buttons[row][column].grid(row=row,column=column)
+        
+        
+
+root.mainloop()
